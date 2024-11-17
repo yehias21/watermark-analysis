c1d613bce0ddf7f7cbd229532cfde573e433a9f3
yehias21 <yehia.salah.ms@alexu.edu.eg>
2024-11-17 14:05:14 +0000

    editted

--- watermarks/DwtDct.py
+++ watermarks/DwtDct.py
@@ -1,16 +1,67 @@
 import numpy as np
 import cv2
+from PIL import Image
 import pywt
 
+
 class WatermarkEmbedder:
-    def __init__(self, watermarks=[], wmLen=8, scales=[0, 36, 36], block=4, use_svd=False):
-        self._watermarks = watermarks
-        self._wmLen = wmLen
+    def __init__(self, scales=[0, 36, 36], block=4, use_svd=False):
         self._scales = scales
         self._block = block
         self._use_svd = use_svd
 
-    def encode(self, bgr):
+    def encode(self, images, watermarks):
+        """
+        Encodes watermarks into a batch of images.
+
+        Args:
+            images (list): List of Pillow images to encode.
+            watermarks (numpy.ndarray): Batch of watermarks with shape (n, 32).
+
+        Returns:
+            list: List of encoded Pillow images.
+        """
+        batch_size = len(images)
+        if batch_size != watermarks.shape[0]:
+            raise ValueError("Number of images and watermarks must match in the batch.")
+
+        encoded_images = []
+        for idx, image in enumerate(images):
+            watermark = watermarks[idx]
+            bgr = np.array(image)
+            encoded_bgr = self._encode_single(bgr, watermark)
+            encoded_image = Image.fromarray(encoded_bgr)
+            encoded_images.append(encoded_image)
+        return encoded_images
+
+    def decode(self, images):
+        """
+        Decodes watermarks from a batch of images.
+
+        Args:
+            images (list): List of Pillow images to decode.
+
+        Returns:
+            numpy.ndarray: Decoded watermarks with shape (n, 32).
+        """
+        decoded_watermarks = []
+        for image in images:
+            bgr = np.array(image)
+            bits = self._decode_single(bgr)
+            decoded_watermarks.append(bits)
+        return np.array(decoded_watermarks)
+
+    def _encode_single(self, bgr, watermark):
+        """
+        Encodes a single image with a single watermark.
+
+        Args:
+            bgr (numpy.ndarray): BGR image as a numpy array.
+            watermark (numpy.ndarray): 1D array of watermark bits with length 32.
+
+        Returns:
+            numpy.ndarray: Encoded BGR image.
+        """
         (row, col, channels) = bgr.shape
         yuv = cv2.cvtColor(bgr, cv2.COLOR_BGR2YUV)
 
@@ -19,18 +70,27 @@ class WatermarkEmbedder:
                 continue
 
             ca1, (h1, v1, d1) = pywt.dwt2(yuv[:row // 4 * 4, :col // 4 * 4, channel], 'haar')
-            self.encode_frame(ca1, self._scales[channel])
+            self.encode_frame(ca1, watermark, self._scales[channel])
 
             yuv[:row // 4 * 4, :col // 4 * 4, channel] = pywt.idwt2((ca1, (v1, h1, d1)), 'haar')
 
         bgr_encoded = cv2.cvtColor(yuv, cv2.COLOR_YUV2BGR)
         return bgr_encoded
 
-    def decode(self, bgr):
+    def _decode_single(self, bgr):
+        """
+        Decodes a single image to extract the watermark.
+
+        Args:
+            bgr (numpy.ndarray): BGR image as a numpy array.
+
+        Returns:
+            numpy.ndarray: Decoded watermark bits as a 1D array of length 32.
+        """
         (row, col, channels) = bgr.shape
         yuv = cv2.cvtColor(bgr, cv2.COLOR_BGR2YUV)
 
-        scores = [[] for _ in range(self._wmLen)]
+        scores = [[] for _ in range(32)]
         for channel in range(2):
             if self._scales[channel] <= 0:
                 continue
@@ -39,17 +99,17 @@ class WatermarkEmbedder:
             scores = self.decode_frame(ca1, self._scales[channel], scores)
 
         avgScores = list(map(lambda l: np.array(l).mean(), scores))
-        bits = (np.array(avgScores) * 255 > 127)
+        bits = (np.array(avgScores) * 255 > 127).astype(int)
         return bits
 
-    def encode_frame(self, frame, scale):
+    def encode_frame(self, frame, watermark, scale):
         (row, col) = frame.shape
         num = 0
         for i in range(row // self._block):
             for j in range(col // self._block):
                 block = frame[i * self._block: i * self._block + self._block,
                               j * self._block: j * self._block + self._block]
-                wmBit = self._watermarks[num % self._wmLen]
+                wmBit = watermark[num % 32]
 
                 if self._use_svd:
                     diffusedBlock = self.diffuse_dct_svd(block, wmBit, scale)
@@ -75,7 +135,7 @@ class WatermarkEmbedder:
                 else:
                     score = self.infer_dct_matrix(block, scale)
 
-                wmBit = num % self._wmLen
+                wmBit = num % 32
                 scores[wmBit].append(score)
                 num += 1
 
